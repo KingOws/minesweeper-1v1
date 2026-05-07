efcef5736c829ee85c22cbcb3a716dbdac9ab4c9
SNCSamiTek <samorozcoestrada@gmail.com>
2026-05-07 15:40:39 -0700

    added ostream operator for debugging pactors/available games

--- src/net/pactor.h
+++ src/net/pactor.h
@@ -23,5 +23,43 @@ class pactor : public std::vector<T>{
             return packet;
         }
 
+        friend std::ostream& operator<<(std::ostream& os, pactor& p){
+            for(typename pactor<T>::iterator it = p.begin(); it != p.end(); it++){
+                os << *it;
+            }
+            return os;
+        }
+
     private:
+};
+
+
+template<>
+class pactor<int> : public std::vector<int>{
+    public:
+        friend sf::Packet& operator<<(sf::Packet& packet, pactor& p){
+            packet << p.size();
+            for(auto it = p.begin(); it != p.end(); it++)
+                packet << *it;
+            return packet;
+        }
+
+        friend sf::Packet& operator>>(sf::Packet& packet, pactor& p){
+            int size;
+            if(!(packet >> size)) return packet;
+            p.resize(size);
+            for(auto it = p.begin(); it != p.end(); it++)
+                packet >> *it;
+            return packet;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, pactor& p){
+            os << "Size is: ";
+            os << p.size();
+            os << std::endl;
+            for(auto it = p.begin(); it != p.end(); it++)
+                os << *it << " ";
+            os << std::endl;
+            return os;
+        }
 };
